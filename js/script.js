db8277e2e0742b0aedaad2f1e5487b089b53401a
dpals103 <dpals103@gmail.com>
2025-04-28 10:13:15 +0900

    작업파일 동기화화

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (e) => {
 
-    // work list 템플릿화
+    //Work List (Menu)
     class workList extends HTMLElement {
         connectedCallback() {
             this.innerHTML = `<section class="detail-list">
@@ -12,16 +12,6 @@ document.addEventListener('DOMContentLoaded', (e) => {
                                 <p class="list-title">Featured Work</p>
                             </li>
                             <li data-id="1">
-                                <a href="notdefined">
-                                    <span>2024</span>
-                                    <span>MORII Magazine</span>
-                                    <span>Editorial</span>
-                                    <span class="icon">
-                                        <img src="assets/images/icons/Arrow right.svg" alt="">
-                                    </span>
-                                </a>
-                            </li>
-                            <li data-id="2">
                                 <a href="ahole.html">
                                     <span>2024</span>
                                     <span>AHOLE</span>
@@ -31,18 +21,28 @@ document.addEventListener('DOMContentLoaded', (e) => {
                                     </span>
                                 </a>
                             </li>
-                            <li data-id="3">
-                                <a href="notdefined">
+                            <li data-id="2">
+                                <a href="morii.html">
                                     <span>2024</span>
-                                    <span>COMM DSGN</span>
+                                    <span>MORII Magazine</span>
                                     <span>Editorial</span>
                                     <span class="icon">
                                         <img src="assets/images/icons/Arrow right.svg" alt="">
                                     </span>
                                 </a>
                             </li>
+                            <li data-id="3">
+                                <a href="commdsgn.html">
+                                    <span>2024</span>
+                                    <span>COMM DSGN</span>
+                                    <span>Branding</span>
+                                    <span class="icon">
+                                        <img src="assets/images/icons/Arrow right.svg" alt="">
+                                    </span>
+                                </a>
+                            </li>
                             <li data-id="4">
-                                <a href="notdefined">
+                                <a href="heynike.html">
                                     <span>2024</span>
                                     <span>Hey, Nike!</span>
                                     <span>UX/UI</span>
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
                                 <p class="list-title">All Work</p>
                             </li>
                             <li data-id="5">
-                                <a href="notdefined">
+                                <a href="airplan.html">
                                     <span>2024</span>
                                     <span>Airplan</span>
                                     <span>UX/UI</span>
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
                                 </a>
                             </li>
                             <li data-id="6">
-                                <a href="notdefined">
+                                <a href="p2p.html">
                                     <span>2024</span>
                                     <span>Crafty Critter</span>
                                     <span>UX/UI</span>
@@ -77,7 +77,17 @@ document.addEventListener('DOMContentLoaded', (e) => {
                                 </a>
                             </li>
                             <li data-id="7">
-                                <a href="notdefined">
+                                <a href="pedaltopetal.html">
+                                    <span>2023</span>
+                                    <span>Pedal to Petal</span>
+                                    <span>Branding</span>
+                                    <span class="icon">
+                                        <img src="assets/images/icons/Arrow right.svg" alt="">
+                                    </span>
+                                </a>
+                            </li>
+                            <li data-id="8">
+                                <a href="yearbook.html">
                                     <span>2023</span>
                                     <span>Yearbook Poster</span>
                                     <span>Editorial</span>
@@ -86,18 +96,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
                                     </span>
                                 </a>
                             </li>
-                            <li data-id="8">
-                                <a href="notdefined">
-                                    <span>2023</span>
-                                    <span>MORII Magazine</span>
-                                    <span>Editorial</span>
-                                    <span class="icon">
-                                        <img src="assets/images/icons/Arrow right.svg" alt="">
-                                    </span>
-                                </a>
-                            </li>
                             <li data-id="9">
-                                <a href="notdefined">
+                                <a href="empty.hyml">
                                     <span>2022</span>
                                     <span>SOLE Magazine</span>
                                     <span>Editorial</span>
@@ -107,7 +107,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
                                 </a>
                             </li>
                             <li data-id="10">
-                                <a href="notdefined">
+                                <a href="empty2.html">
                                     <span>2022</span>
                                     <span>COMM DSGN</span>
                                     <span>Branding</span>
@@ -125,19 +125,18 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
     customElements.define('work-list', workList);
 
-
-    // m-nav 템플릿화
-    class mNav extends HTMLElement{
+    //m-nav template
+    class mNav extends HTMLElement {
         connectedCallback(){
             this.innerHTML = `<div class="m-bottom-menu">
             <button class="m-menu-btn">Menu</button>
             <nav class="m-nav">
                 <button class="m-nav-close">
-                    <img src="assets/images/icons/ico_close.svg" alt="nav close">
+                    <img src="assets/images/icons/close.svg" alt="nav close">
                 </button>
                 <ul>
                     <li>
-                        <a href="work.html">Work</a>
+                        <a href="index.html">Work</a>
                     </li>
                     <li>
                         <a href="archive.html">Archive</a>
@@ -147,8 +146,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
                     </li>
                 </ul>
                 <div>
-                    <a href="">Instagram <img src="assets/images/icons/ico_arrow-dig.svg" alt="go instagram"></a>
-                    <a href="">LinkedIn <img src="assets/images/icons/ico_arrow-dig.svg" alt="go LinkedIn"></a>
+                    <a href="">Instagram <img src="assets/images/icons/arrow-dig.svg" alt="go instagram"></a>
+                    <a href="">Linkedin <img src="assets/images/icons/arrow-dig.svg" alt="go Linkedin"></a>
                 </div>
             </nav>
         </div>`
@@ -157,108 +156,96 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
     customElements.define('m-nav', mNav);
 
+       // 해당 페이지에서 리스트 요소 활성화
+       const workDetail = document.querySelectorAll(".detail .list-group > ul > li > a");
+       const currentPath = window.location.pathname; // 현재 페이지 경로
+   
+       workDetail.forEach((link) => {
+           const linkPath = new URL(link.href, window.location.origin).pathname;
+           if (linkPath === currentPath) {
+               link.parentNode.classList.add("active");
+           }
+       });
 
 
-
-
-    // 해당 페이지에서 리스트 요소 활성화
-    const workDetail = document.querySelectorAll(".detail .list-group > ul > li > a");
-    const currentPath = window.location.pathname; // 현재 페이지 경로
-
-    workDetail.forEach((link) => {
-        const linkPath = new URL(link.href, window.location.origin).pathname;
-        if (linkPath === currentPath) {
-            link.parentNode.classList.add("active");
-        }
-    });
-
-
-    // 이미지 hover시 리스트도 hover 효과
-    document.querySelectorAll(".grid .grid-item").forEach((img) => {
+        // image hover시 list도 hover effect
+        document.querySelectorAll(".grid .grid-item").forEach((img) => {
         img.addEventListener("mouseenter", function () {
             const id = this.dataset.id; // 이미지의 data-id 값 가져오기
             document.querySelector(`.list-group li[data-id="${id}"]`)?.classList.add("hover");
         });
-
+      
         img.addEventListener("mouseleave", function () {
-            const id = this.dataset.id;
-
-            // hover 클래스 제거
-            document.querySelector(`.list-group li[data-id="${id}"]`)?.classList.remove("hover");
+          const id = this.dataset.id;
+          
+          // hover class 제거
+          document.querySelector(`.list-group li[data-id="${id}"]`)?.classList.remove("hover");
         });
 
+        // tablet touch hover effect 
+       let touched = false;
 
-        // 태블릿 터치 시 hover 효과
-        
-        let touched = false;
-
-        img.addEventListener('touchstart', (e) => {
-            if (!touched) {
-              e.preventDefault(); // 첫 터치에 링크 이동 막음
-              img.classList.add('active');
-              touched = true;
-        
-              // 다른 요소를 터치하면 상태 초기화
-              document.addEventListener('touchstart', function resetTouch(event) {
-                if (!link.contains(event.target)) {
-                  link.classList.remove('active');
-                  touched = false;
-                  document.removeEventListener('touchstart', resetTouch);
-                }
-              });
-            } else {
-              // 두 번째 터치면 링크 이동
-              link.classList.remove('active');
-              touched = false;
-              window.location.href = img.firstElementChild('a').getAttribute('href');
-            }
-          });
-    
+       img.addEventListener('touchstart', (e) => {
+           if (!touched) {
+             e.preventDefault(); // 첫 터치에 링크 이동 막음
+             img.classList.add('active');
+             touched = true;
        
+             // 다른 요소를 터치하면 상태 초기화
+             document.addEventListener('touchstart', function resetTouch(event) {
+               if (!link.contains(event.target)) {
+                 link.classList.remove('active');
+                 touched = false;
+                 document.removeEventListener('touchstart', resetTouch);
+               }
+             });
+           } else {
+             // 두 번째 터치면 링크 이동
+             link.classList.remove('active');
+             touched = false;
+             window.location.href = img.firstElementChild('a').getAttribute('href');
+           }
+         });
+   
 
     });
-
-
-    // Masonry를 감싼 스크롤 컨테이너
     const scrollContainer = document.querySelector(".work .grid");
 
     document.querySelectorAll("work-list .list-group li").forEach((list) => {
         list.addEventListener("mouseenter", function () {
             const id = this.dataset.id; // 이미지의 data-id 값 가져오기
-            const targetImg = document.querySelector(`.grid .grid-item[data-id="${id}"] a`) //타겟이미지
-            targetImg?.classList.add("active"); // 이미지 영역에 클래스 추가하여 hover 효과 
+            const targetImg = document.querySelector(`.grid .grid-item[data-id="${id}"] a`)
+            targetImg?.classList.add("active");
 
             const containerTop = scrollContainer.getBoundingClientRect().top; // 스크롤 컨테이너가 브라우저 기준에서 얼마나 아래에 있는지
             const imageTop = targetImg.getBoundingClientRect().top; // 타겟이미지가 브라우저 기준에서 얼마나 아래에 있는지지
 
             const offset = imageTop - containerTop + scrollContainer.scrollTop - 100;
 
-
             scrollContainer.scrollTo({
                 top: offset,
                 behavior: "smooth"
             });
         });
-
+      
         list.addEventListener("mouseleave", function () {
-            const id = this.dataset.id;
-
-            // hover 클래스 제거
-            document.querySelector(`.grid .grid-item[data-id="${id}"] a`)?.classList.remove("active");
+          const id = this.dataset.id;
+          
+          // hover class 제거
+          document.querySelector(`.grid .grid-item[data-id="${id}"] a`)?.classList.remove("active");
         });
     });
-
 
 
     //about페이지에서 스크롤시 하단 요소 애니메이션 작업
     let lastScrollTop = 0;
     let ticking = false;
     const targetElement = document.querySelector(".hide-bottom");
-
+    
     function onScroll() {
         let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-        if (currentScroll - lastScrollTop > 3) {
+        if (currentScroll - lastScrollTop > 3) { 
             targetElement.style.transform = "translateY(100%)";
             targetElement.style.opacity = "0";
         } else if (lastScrollTop - currentScroll > 3) {
@@ -277,16 +264,16 @@ document.addEventListener('DOMContentLoaded', (e) => {
         }
     });
 
-
-
     const items = document.querySelectorAll('.grid-item');
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('show');
-                //observer.unobserve(entry.target); // 한 번만 실행
-            } else {
+                // observer.unobserve(entry.target); // 한 번만 실행
+            }
+
+            else {
                 entry.target.classList.remove('show');
             }
         });
@@ -295,8 +282,6 @@ document.addEventListener('DOMContentLoaded', (e) => {
     });
 
     items.forEach((item) => observer.observe(item));
-
-
 
     // mobile bottom menu
     const mMenuBtn = document.querySelector('.m-menu-btn');
@@ -310,4 +295,5 @@ document.addEventListener('DOMContentLoaded', (e) => {
     mMenuClose.addEventListener('click', () => {
          mMenuNav.classList.remove('active');
     });
+
 });
